006cb243461d64aaed5dc05b9ccef52f03bca17c
Harpal Gupta <harpal.gupta@ensek.co.uk>
2019-12-31 13:40:05 +0000

    Day2 Complete

---
+++ AdventOfCode2019/Day2Tests.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace AdventOfCode2019
+{
+    [TestFixture]
+    public class Day2Tests
+    {
+        private Day2_1202ProgramAlarm _program;
+
+        [SetUp]
+        public void Setup()
+        {
+            _program = new Day2_1202ProgramAlarm();
+        }
+
+        [Test]
+        public void HaltCorrectly_ReturnsInputAsOutput()
+        {
+            int[] instructions = {99};
+
+            Assert.AreEqual(new[] {99}, _program.ProcessInstructions(instructions));
+        }
+
+        [Test]
+        public void HaltCorrectly_ReturnsInputAsOutputEvenInputAfterHaltOpCode()
+        {
+            int[] instructions = {99, 1, 2, 3, 4, 5, 6};
+
+            Assert.AreEqual(new[] {99, 1, 2, 3, 4, 5, 6}, _program.ProcessInstructions(instructions));
+        }
+
+        [Test]
+        public void OpCode1_AddsNumberInPosition0TwiceAndWriteOutputInPosition0()
+        {
+            int[] instructions = {1, 0, 0, 0, 99};
+
+            Assert.AreEqual(new[] {2, 0, 0, 0, 99}, _program.ProcessInstructions(instructions));
+        }
+
+        [Test]
+        public void OpCode1_AddsNumberAndWriteOutputsBasedOnFollowing3Inputs()
+        {
+            int[] instructions = {1, 5, 3, 2, 99, 18};
+
+            Assert.AreEqual(new[] {1, 5, 20, 2, 99, 18}, _program.ProcessInstructions(instructions));
+        }
+
+        [Test]
+        public void SubsequentOpCode1_DoesTheJobUntil99()
+        {
+            int[] instructions = {1, 2, 3, 1, 1, 8, 9, 5, 99, 5};
+
+            Assert.AreEqual(new[] {1, 4, 3, 1, 1, 104, 9, 5, 99, 5}, _program.ProcessInstructions(instructions));
+        }
+
+        [Test]
+        public void SubsequentOpCode1_CanChangeOtherSplitsUntil99()
+        {
+            int[] instructions = {1, 0, 1, 4, 0, 8, 9, 1, 99, 5};
+
+            Assert.AreEqual(new[] {1, 104, 1, 4, 1, 8, 9, 1, 99, 5}, _program.ProcessInstructions(instructions));
+        }
+
+        [Test]
+        public void OpCode2_Multiply()
+        {
+            int[] instructions = {2, 0, 2, 1, 99, 5};
+
+            Assert.AreEqual(new[] {2, 4, 2, 1, 99, 5}, _program.ProcessInstructions(instructions));
+        }
+
+        [Test]
+        public void Test1()
+        {
+            int[] instructions = {1,1,1,4,99,5,6,0,99};
+
+            Assert.AreEqual(new[] {30,1,1,4,2,5,6,0,99}, _program.ProcessInstructions(instructions));
+        }
+        
+        [Test]
+        public void Test2()
+        {
+            int[] instructions = {1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,1,10,19,1,19,5,23,2,23,6,27,1,27,5,31,2,6,31,35,1,5,35,39,2,39,9,43,1,43,5,47,1,10,47,51,1,51,6,55,1,55,10,59,1,59,6,63,2,13,63,67,1,9,67,71,2,6,71,75,1,5,75,79,1,9,79,83,2,6,83,87,1,5,87,91,2,6,91,95,2,95,9,99,1,99,6,103,1,103,13,107,2,13,107,111,2,111,10,115,1,115,6,119,1,6,119,123,2,6,123,127,1,127,5,131,2,131,6,135,1,135,2,139,1,139,9,0,99,2,14,0,0};
+
+            var state = _program.ProcessInstructions(instructions, 12, 2);
+            Assert.AreEqual(5866663, state[0]);
+        }
+
+        [Test]
+        public void BreakingTeeeest()
+        {
+            int[] instructions = {1, 0, 0, 0, 99, 0, 0, 0, 1, 0, 0, 0, 99};
+
+            Assert.AreEqual(new[] {2, 0, 0, 0, 99, 0, 0, 0, 1, 0, 0, 0, 99}, _program.ProcessInstructions(instructions));
+        }
+
+        [Test]
+        public void ProcessDoesNotModifyInputArray()
+        {
+            int[] instructions = {1, 0, 0, 0, 99};
+            _program.ProcessInstructions(instructions);
+            
+            Assert.AreEqual(new[] {1, 0, 0, 0, 99}, instructions);
+        }
+        
+        [Test]
+        public void LoopForNounAndVerb()
+        {
+            int[] instructions = {1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,1,10,19,1,19,5,23,2,23,6,27,1,27,5,31,2,6,31,35,1,5,35,39,2,39,9,43,1,43,5,47,1,10,47,51,1,51,6,55,1,55,10,59,1,59,6,63,2,13,63,67,1,9,67,71,2,6,71,75,1,5,75,79,1,9,79,83,2,6,83,87,1,5,87,91,2,6,91,95,2,95,9,99,1,99,6,103,1,103,13,107,2,13,107,111,2,111,10,115,1,115,6,119,1,6,119,123,2,6,123,127,1,127,5,131,2,131,6,135,1,135,2,139,1,139,9,0,99,2,14,0,0};
+            var result = _program.LoopNounVerbProcessUntilMatchedReturningResult(instructions, 19690720);
+
+            Assert.AreEqual(4259, result);
+        }
+    }
+}
---
+++ AdventOfCode2019/Day2_1202ProgramAlarm.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace AdventOfCode2019
+{
+    public class Day2_1202ProgramAlarm
+    {
+        private readonly ProgramAlarm _programAlarm = new ProgramAlarm();
+        
+        public int[] ProcessInstructions(int[] instructions)
+        {
+            _programAlarm.Process(instructions);
+
+            return _programAlarm.State;
+        }
+        
+        public int[] ProcessInstructions(int[] instructions, int noun, int verb)
+        {
+            instructions = instructions.ToArray();
+            
+            instructions[1] = noun;
+            instructions[2] = verb;
+
+            return ProcessInstructions(instructions);
+        }
+
+        public int LoopNounVerbProcessUntilMatchedReturningResult(int[] instructions, int matchValue)
+        {
+            instructions = instructions.ToArray();
+
+            for (int noun = 0; noun <= 99; noun++)
+            {
+                for (int verb = 0; verb <= 99; verb++)
+                {
+                    int[] state = ProcessInstructions(instructions, noun, verb);
+
+                    if (state[0] == matchValue)
+                    {
+                        return 100 * noun + verb;
+                    }
+                }
+            }
+
+            throw new Exception("Match not found");
+        }
+
+        private class ProgramAlarm
+        {
+            public int[] State { get; private set; }
+            private int _pointer;
+            private Dictionary<int, Action> _operations;
+            private bool _isProcessing;
+
+            private int OpCode => State[_pointer];
+
+            public ProgramAlarm()
+            {
+                BuildOperations();
+            }
+
+            public void Process(int[] instructions)
+            {
+                State = instructions.ToArray();
+
+                for (_pointer = 0, _isProcessing = true; _isProcessing;)
+                {
+                    if (_operations.ContainsKey(OpCode))
+                    {
+                        _operations[OpCode]();
+                    }
+                }
+            }
+
+            private void Sum()
+            {
+                State[Param(3)] = State[Param(1)] + State[Param(2)];
+                MovePointer(4);
+            }
+
+            private void Product()
+            {
+                State[Param(3)] = State[Param(1)] * State[Param(2)];
+                MovePointer(4);
+            }
+
+            private void Stop()
+            {
+                _isProcessing = false;
+            }
+
+            private int Param(int number)
+            {
+                return State[_pointer + number];
+            }
+
+            private void MovePointer(int positions)
+            {
+                _pointer += positions;
+            }
+
+            private void BuildOperations()
+            {
+                _operations = new Dictionary<int, Action>
+                {
+                    { 1, Sum },
+                    { 2, Product },
+                    { 99, Stop }
+                };
+            }
+        }
+    }
+}
