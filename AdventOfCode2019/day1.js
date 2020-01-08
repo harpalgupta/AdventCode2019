5d5bfb8161c03be3985edc2b1dbfc960df12aa8c
Harpal Gupta <harpal.gupta@gmail.com>
2020-01-08 18:56:58 +0000

    add day1

---
+++ AdventOfCode2019/day1.js
@@ -0,0 +1,29 @@
+var fs = require("fs");
+
+var contents = fs
+  .readFileSync("input.txt", "utf8")
+  .toString()
+  .split("\n");
+
+var massCalculate = mass => {
+  return Math.floor(mass / 3) - 2;
+};
+
+const massCalculateWithFuel = mass => {
+  let total = 0;
+  var tmpMass = mass;
+  while (tmpMass > 2) {
+    tmpMass = massCalculate(tmpMass);
+    if (tmpMass > 0) total += tmpMass;
+  }
+  return total;
+};
+
+let part1total = 0;
+let part2total = 0;
+
+contents.forEach(element => {
+  part1total += massCalculate(element);
+  part2total += massCalculateWithFuel(element);
+});
+console.log(part1total, part2total);
